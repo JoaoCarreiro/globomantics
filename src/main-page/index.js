a0302dcf56ccf94191a8f03c5045ddf64a6b88f8
JoaoCarreiro <92582377+JoaoCarreiro@users.noreply.github.com>
2021-11-23 15:45:59 +0000

    Update app

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,11 +1,22 @@
-import Header from './components/header';
-import logo from './logo.svg';
-import './main-page.css';
+import Header from "./components/header";
+import logo from "./logo.svg";
+import "./main-page.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
   return (
-    <div className="App">
-      <Header/>
+    <div className="container">
+      <Header subtitle="Providing houses all over the world" />
     </div>
   );
 }
